04ae38f44194cefdb42c5a6166e600cf77782d69
waeldgheim <waeldgheim@hotmail.com>
2024-08-01 19:45:07 +0300

    Injected viewmodels in composables

--- app/src/main/java/com/example/musicapp/screens/screena/ScreenAViewModel.kt
+++ app/src/main/java/com/example/musicapp/screens/screena/ScreenAViewModel.kt
@@ -3,5 +3,5 @@ package com.example.musicapp.screens.screena
 import androidx.lifecycle.ViewModel
 
 class ScreenAViewModel : ViewModel() {
-
+    val test = "From viewModel"
 }
--- app/src/main/java/com/example/musicapp/screens/screenb/ScreenB.kt
+++ app/src/main/java/com/example/musicapp/screens/screenb/ScreenB.kt
@@ -1,9 +1,12 @@
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.example.musicapp.screens.screenb.ScreenBViewModel
 
 @Composable
 fun ScreenBContent(buttonId: String?) {
+    val viewModel = hiltViewModel<ScreenBViewModel>()
     Text(
         text = "Button ID: $buttonId",
         modifier = Modifier
