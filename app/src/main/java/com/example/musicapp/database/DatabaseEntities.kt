59e5d83d03fcabe54930de034d4113e901591dd6
RanimeeKhoury <khouryranime@gmail.com>
2024-08-03 11:30:59 +0300

    Created the DatabaseEntities (DatabaseAlbum & Genre & AlbumGenreJoin)

---
+++ app/src/main/java/com/example/musicapp/database/DatabaseEntities.kt
@@ -0,0 +1,35 @@
+package com.example.musicapp.database
+
+import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.PrimaryKey
+import java.util.Date
+
+@Entity(tableName = "albums")
+data class DatabaseAlbum(
+    @PrimaryKey(autoGenerate = true) val id: Long = 0,
+    val name: String,
+    val imageUrl: String,
+    val artistName: String,
+    val releaseDate: Date,
+)
+
+@Entity(tableName = "genres")
+data class Genre(
+    @PrimaryKey(autoGenerate = true) val id: Long = 0,
+    val name: String,
+    val url: String
+)
+
+@Entity(
+    tableName = "album_genre_join",
+    primaryKeys = ["albumId", "genreId"],
+    foreignKeys = [
+        ForeignKey(entity = DatabaseAlbum::class, parentColumns = ["id"], childColumns = ["albumId"]),
+        ForeignKey(entity = Genre::class, parentColumns = ["id"], childColumns = ["genreId"])
+    ]
+)
+data class AlbumGenreJoin(
+    val albumId: Long,
+    val genreId: Long
+)
