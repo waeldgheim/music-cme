667ad261c13935a2aa4567a140872fff1b14b19c
waeldgheim <waeldgheim@hotmail.com>
2024-08-05 15:28:52 +0300

    Injected service into viewModel of screen A

--- app/src/main/java/com/example/musicapp/screens/screena/ScreenAViewModel.kt
+++ app/src/main/java/com/example/musicapp/screens/screena/ScreenAViewModel.kt
@@ -1,7 +1,13 @@
 package com.example.musicapp.screens.screena
 
 import androidx.lifecycle.ViewModel
+import com.example.musicapp.network.MusicAppService
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
-class ScreenAViewModel : ViewModel() {
+@HiltViewModel
+class ScreenAViewModel @Inject constructor (
+    private val musicAppService: MusicAppService
+): ViewModel() {
     val test = "From viewModel"
 }
