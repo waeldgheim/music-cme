a16f0692a7ebbc0d1ee1fc90544aa209d5bd682e
waeldgheim <waeldgheim@hotmail.com>
2024-08-06 13:11:04 +0300

    Implemented dao

--- app/src/main/java/com/example/musicapp/database/DAOs.kt
+++ app/src/main/java/com/example/musicapp/database/DAOs.kt
@@ -1,6 +1,8 @@
 package com.example.musicapp.database
 
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 
 @Dao
@@ -10,22 +12,7 @@ interface AlbumDao {
 
     @Query("SELECT * FROM albums")
     suspend fun getAllAlbums(): List<DatabaseAlbum>
-}
 
-@Dao
-interface GenreDao {
-    @Query("SELECT * FROM genres WHERE id IN (:genreIds)")
-    fun getGenresByIds(genreIds: List<Long>): List<DatabaseGenre>
-
-    // MightRemoveLater
-    @Query("SELECT * FROM genres")
-    suspend fun getAllGenres(): List<DatabaseGenre>
-}
-
-@Dao
-interface AlbumGenreJoinDao {
-    @Query("SELECT genreId FROM album_genre_join WHERE albumId = :albumId")
-    fun getGenreIdsByAlbumId(albumId: Long): List<Long>
-
-
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertAll(vararg albums: DatabaseAlbum)
 }
