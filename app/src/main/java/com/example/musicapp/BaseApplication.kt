de40a6955460cde90ed9d48b63570cf194698a16
Ranime <khouryranime@gmail.com>
2024-08-07 10:12:37 +0300

    Created Realm object in BaseApplication

--- app/src/main/java/com/example/musicapp/BaseApplication.kt
+++ app/src/main/java/com/example/musicapp/BaseApplication.kt
@@ -1,9 +1,23 @@
 package com.example.musicapp
 
 import android.app.Application
+import com.example.musicapp.realm.Album
 import dagger.hilt.android.HiltAndroidApp
+import io.realm.kotlin.Realm
+import io.realm.kotlin.RealmConfiguration
 
 @HiltAndroidApp
 class BaseApplication: Application() {
+    companion object {
+        lateinit var realm: Realm
+    }
 
+    override fun onCreate() {
+        super.onCreate()
+        realm = Realm.open(
+            configuration = RealmConfiguration.create(
+                schema = setOf(Album::class)
+            )
+        )
+    }
 }
