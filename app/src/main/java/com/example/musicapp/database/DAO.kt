1d05798706559d06868ea3d07f2df4082d05fa74
waeldgheim <waeldgheim@hotmail.com>
2024-08-07 16:10:30 +0300

    Created DAO

---
+++ app/src/main/java/com/example/musicapp/database/DAO.kt
@@ -0,0 +1,19 @@
+package com.example.musicapp.database
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface AlbumDao {
+    @Query("SELECT * FROM albums WHERE id = :id")
+    fun getAlbumById(id: String): DatabaseAlbum?
+
+    @Query("SELECT * FROM albums")
+    fun getAllAlbums(): Flow<List<DatabaseAlbum>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertAll(vararg albums: DatabaseAlbum)
+}
