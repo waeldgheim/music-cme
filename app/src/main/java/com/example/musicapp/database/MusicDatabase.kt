4c9614d28f9806bef20f71ae4797648bb71f262e
waeldgheim <waeldgheim@hotmail.com>
2024-08-07 10:31:27 +0300

    Created database module

--- app/src/main/java/com/example/musicapp/database/MusicDatabase.kt
+++ app/src/main/java/com/example/musicapp/database/MusicDatabase.kt
@@ -1,24 +1,9 @@
 package com.example.musicapp.database
 
-import android.content.Context
 import androidx.room.Database
-import androidx.room.Room
 import androidx.room.RoomDatabase
 
 @Database(entities = [DatabaseAlbum::class], version = 1)
 abstract class MusicDatabase : RoomDatabase() {
     abstract val albumDao: AlbumDao
-}
-
-private lateinit var INSTANCE: MusicDatabase
-
-fun getDatabase(context:Context):MusicDatabase{
-    synchronized(MusicDatabase::class.java){
-        if (!::INSTANCE.isInitialized) {
-            INSTANCE = Room.databaseBuilder(context.applicationContext,
-                MusicDatabase::class.java,
-                "albumsDatbase").build()
-        }
-    }
-    return INSTANCE
 }
