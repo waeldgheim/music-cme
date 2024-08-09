2756bedabb53dec3419fbecb76f9688bfdb80556
waeldgheim <waeldgheim@hotmail.com>
2024-08-09 12:15:25 +0300

    Handled case when status is error

--- app/src/main/java/com/example/musicapp/screens/screena/ScreenA.kt
+++ app/src/main/java/com/example/musicapp/screens/screena/ScreenA.kt
@@ -2,6 +2,7 @@ package com.example.musicapp.screens.screena
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
@@ -13,11 +14,15 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Refresh
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.graphics.asImageBitmap
@@ -58,9 +63,20 @@ fun ScreenAContent(navController: NavController) {
         }
 
         ApiStatus.ERROR -> {
-            Text(
-                text = "An error occurred.",
-            )
+            Box(
+                modifier = Modifier
+                    .fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                Icon(
+                    imageVector = Icons.Filled.Refresh,
+                    contentDescription = "Error icon",
+                    modifier = Modifier
+                        .size(200.dp)
+                        .padding(16.dp)
+                        .clickable {viewModel.refresh()}
+                )
+            }
         }
 
         ApiStatus.LOADING -> {
