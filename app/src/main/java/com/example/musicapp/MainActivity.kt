5a6560c335d7399bd2c453c5f873bfd77ec47039
Ranime <khouryranime@gmail.com>
2024-08-16 09:42:36 +0300

    Used shared prefrences to store the user color (initialized the instance of the shared prefrences in the main activity using a Theme object, and used StateFlow in ViewModel to track changes from the user)

--- app/src/main/java/com/example/musicapp/MainActivity.kt
+++ app/src/main/java/com/example/musicapp/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.navigation.compose.rememberNavController
 import com.example.musicapp.ui.theme.MusicAppTheme
+import com.example.musicapp.ui.theme.Theme
 import dagger.hilt.android.AndroidEntryPoint
 
 
@@ -12,8 +13,9 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Theme.initialize(this)
         setContent {
-            MusicAppTheme {
+            MusicAppTheme(color = Theme.getTColor()){
                 val navController = rememberNavController()
                 MusicNavHost(navController = navController)
             }
--- app/src/main/java/com/example/musicapp/ui/theme/Theme.kt
+++ app/src/main/java/com/example/musicapp/ui/theme/Theme.kt
@@ -1,5 +1,7 @@
 package com.example.musicapp.ui.theme
 
+import android.content.Context
+import android.content.SharedPreferences
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
@@ -9,12 +11,33 @@ import androidx.compose.ui.graphics.Color
 import com.example.musicapp.ui.theme.ColorUtils.darkenColor
 import com.example.musicapp.ui.theme.ColorUtils.lightenColor
 
+
+object Theme {
+    private var color: Long = 0xFF0C3C85
+    private var preferences: SharedPreferences? = null
+
+    fun initialize(context: Context) {
+        preferences = context.getSharedPreferences("SETTINGS",
+            Context.MODE_PRIVATE)
+        color = preferences?.getLong("COLOR", 0xFF0C3C85) ?: 0xFF0C3C85
+    }
+
+    fun getTColor(): Color {
+        return Color(this.color)
+    }
+
+    fun saveColor(color: Long){
+        this.color = color
+        preferences?.edit()?.putLong("COLOR", color)?.apply()
+    }
+}
+
 @Composable
 fun MusicAppTheme(
+    color: Color,
     darkTheme: Boolean = isSystemInDarkTheme(),
     content: @Composable () -> Unit
 ) {
-    val color = Color(0xFF00008B)
     val LightColorPalette = lightColorScheme(
         primary = color,
         secondary = lightenColor(color, 0.3f),
