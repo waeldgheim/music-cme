e8d466e58d0d9fb50ecd1e372a8a2085e2dcd120
Ranime <khouryranime@gmail.com>
2024-08-21 17:36:57 +0300

    Displayed BottomAppBar from the MainActivity

--- app/src/main/java/com/example/musicapp/MainActivity.kt
+++ app/src/main/java/com/example/musicapp/MainActivity.kt
@@ -1,9 +1,21 @@
 package com.example.musicapp
 
+
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.navigation.compose.rememberNavController
+import com.example.musicapp.screens.components.MusicBottomAppBar
 import com.example.musicapp.ui.theme.MusicAppTheme
 import com.example.musicapp.ui.theme.Theme
 import dagger.hilt.android.AndroidEntryPoint
@@ -11,14 +23,48 @@ import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Theme.initialize(this)
         setContent {
-            MusicAppTheme(color = Theme.getTColor()){
+            MusicAppTheme(color = Theme.getTColor()) {
                 val navController = rememberNavController()
-                MusicNavHost(navController = navController)
+                Scaffold(
+                    bottomBar = {
+                        MusicBottomAppBar(navController = navController)
+                    }
+                ) { innerPadding ->
+                    MusicNavHost(
+                        navController = navController,
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
             }
         }
+    }
+}
+
+@Composable
+fun SearchScreen() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text("Search Screen")
+    }
+}
+
+@Composable
+fun SettingsScreen() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text("Settings Screen")
     }
 }
